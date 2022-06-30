783f2474057e1770fe31f35ee675a793ccafd562
RyanKoech <sirryankoech@gmail.com>
2022-06-30 18:52:11 +0300

    Boilerplate to inflate fragment

--- app/src/main/java/com/example/furnitureapp/ProfileFragment.kt
+++ app/src/main/java/com/example/furnitureapp/ProfileFragment.kt
@@ -1,5 +1,32 @@
 package com.example.furnitureapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.furnitureapp.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment()
+class ProfileFragment : Fragment() {
+
+    var _binding : FragmentProfileBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
