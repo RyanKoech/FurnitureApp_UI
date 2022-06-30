971d630cb9a4ff7705468fd228d8b38e7b268564
RyanKoech <sirryankoech@gmail.com>
2022-06-30 19:19:22 +0300

    Implemented logic to save night mode state

--- app/src/main/java/com/example/furnitureapp/SharedPref.kt
+++ app/src/main/java/com/example/furnitureapp/SharedPref.kt
@@ -1,3 +1,27 @@
 package com.example.furnitureapp
 
-class SharedPref
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPref(context: Context) {
+
+    companion object {
+        val  NIGHT_MODE = "Night_Mode"
+    }
+
+    internal lateinit var sharedPref: SharedPreferences
+
+    init {
+        sharedPref = context.getSharedPreferences("filname", Context.MODE_PRIVATE)
+    }
+
+    fun setNightModeState(state: Boolean?){
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor.putBoolean(NIGHT_MODE, state!!)
+        editor.commit()
+    }
+
+    fun getNightModeState() : Boolean? {
+        return sharedPref.getBoolean(NIGHT_MODE, false)
+    }
+}
