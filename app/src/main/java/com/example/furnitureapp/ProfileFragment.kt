fcd516a826bebb6c297977d81b54d5137e31dbfd
RyanKoech <sirryankoech@gmail.com>
2022-06-30 19:20:33 +0300

    Implemented logic to enable changing of night mode state

--- app/src/main/java/com/example/furnitureapp/ProfileFragment.kt
+++ app/src/main/java/com/example/furnitureapp/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.furnitureapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ class ProfileFragment : Fragment() {
 
     var _binding : FragmentProfileBinding? = null
     val binding get() = _binding!!
+    internal lateinit var sharedPref: SharedPref
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,12 +23,29 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        sharedPref = SharedPref((activity as MainActivity).applicationContext)
+        binding.toggleModeSwitch.apply {
+            isChecked = sharedPref.getNightModeState()!!
+            setOnCheckedChangeListener { buttonView, isChecked ->
+
+                sharedPref.setNightModeState(isChecked)
+                restartApp()
+            }
+        }
+
         return view
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun restartApp() {
+        val activity = activity as MainActivity
+        val intent =Intent((activity).applicationContext, MainActivity::class.java)
+        (activity).finish()
+        startActivity(intent)
     }
 
 }
